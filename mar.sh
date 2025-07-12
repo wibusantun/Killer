0ed4b60d5709e370738d97875bdad4a4b3645136
Wibu <100986373+wibusantun@users.noreply.github.com>
2025-07-12 16:57:07 +0700

    mar.sh

--- mar.sh
+++ mar.sh
@@ -86,38 +86,11 @@ apt-get -y --purge remove apache2*;
 apt-get -y --purge remove sendmail*;
 apt-get -y --purge remove bind9*;
 
-#install bbr
-echo 'fs.file-max = 500000
-net.core.rmem_max = 67108864
-net.core.wmem_max = 67108864
-net.core.netdev_max_backlog = 250000
-net.core.somaxconn = 4096
-net.ipv4.tcp_syncookies = 1
-net.ipv4.tcp_tw_reuse = 1
-net.ipv4.tcp_fin_timeout = 30
-net.ipv4.tcp_keepalive_time = 1200
-net.ipv4.ip_local_port_range = 10000 65000
-net.ipv4.tcp_max_syn_backlog = 8192
-net.ipv4.tcp_max_tw_buckets = 5000
-net.ipv4.tcp_fastopen = 3
-net.ipv4.tcp_mem = 25600 51200 102400
-net.ipv4.tcp_rmem = 4096 87380 67108864
-net.ipv4.tcp_wmem = 4096 65536 67108864
-net.core.rmem_max = 4000000
-net.ipv4.tcp_mtu_probing = 1
-net.ipv4.ip_forward = 1
-net.core.default_qdisc = fq
-net.ipv4.tcp_congestion_control = bbr
-net.ipv6.conf.all.disable_ipv6 = 1
-net.ipv6.conf.default.disable_ipv6 = 1
-net.ipv6.conf.lo.disable_ipv6 = 1' >> /etc/sysctl.conf
-sysctl -p;
-
 #install toolkit
 apt-get install libio-socket-inet6-perl libsocket6-perl libcrypt-ssleay-perl libnet-libidn-perl perl libio-socket-ssl-perl libwww-perl libpcre3 libpcre3-dev zlib1g-dev dbus iftop zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr dnsutils sudo at htop iptables bsdmainutils cron lsof lnav -y
 
-#Set Timezone GMT+7
-timedatectl set-timezone Asia/Jakarta;
+#Set Timezone GMT+8
+timedatectl set-timezone Asia/Singapore;
 
 #Install Marzban
 sudo bash -c "$(curl -sL https://github.com/GawrAme/Marzban-scripts/raw/master/marzban.sh)" @ install
@@ -231,10 +204,6 @@ echo -e "Untuk data login dashboard Marzban:
 URL HTTPS : https://${domain}/dashboard 
 username  : ${userpanel}
 password  : ${passpanel}
--=================================-
-Jangan lupa join Channel & Grup Telegram saya juga di
-Telegram Channel: https://t.me/Deki_niswara
-Telegram Group: https://t.me/killervpn_store
 -=================================-" > /root/log-install.txt
 profile
 colorized_echo green "Script telah berhasil di install"
